e9330762b6ba54200f4ad60cfe063de85691be73
tcu93 <tcu93@live.com>
2019-03-28 13:45:25 -0500

    turn off debug

--- project.py
+++ project.py
@@ -345,5 +345,5 @@ def deleteItem(category_id, item_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=5000)
